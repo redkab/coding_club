4a97c40241fadf6d2c6e160b41c8f7c08192fd31
Saketh Indurthi <saketh.indurthi@gmail.com>
2025-09-09 11:50:37 -0400

    add integer_reverse

---
+++ reverse_integer.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int reverse_integer(int x)
+{
+    int rem=0;
+    int reverse = 0;
+    if (x<0)
+    {
+        x = 0-x;
+    }
+
+    while(x<0)
+    {
+        rem = x%10;
+        reverse = 10*reverse + rem;
+        x = x/10;
+    }
+    if(x>0)
+    {
+        return reverse;
+    }
+    if (x <= 0)
+    {
+        return 0 - reverse;
+    }
+}
+
+int main()
+{
+    int x;
+    printf("Enter your number\n");
+    scanf("%d", &x);
+    int a;
+    a = reverse_integer(x);
+    printf("Reverse is %d\n", a);
+}
+
