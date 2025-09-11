c5da843d5f00c9a7eff84a0fca10c580782f0f41
Saketh Indurthi <saketh.indurthi@gmail.com>
2025-09-11 12:28:01 -0400

    update palindrome

--- is_palindrome.c
+++ is_palindrome.c
@@ -3,31 +3,23 @@
 int is_palindrome(int x)
 {
     int rem=0;
-    int reverse=0;
-    if(x<0)
-    {   
-      x = x * -1; 
-    }   
+    int copy = x;
+    int reverse=0; 
+    //printf("Entered is_palindrome with x = %d\n", x);
+    if(x < 0)
+    {
+        return 0;
+    }
 
-    
     while(x > 0)
     {   
-      rem = x%10;
-       reverse = 10*reverse + rem;
-    x = x/10;
+        rem = x % 10;
+        reverse = 10 * reverse + rem;
+        x = x / 10;
+
     }   
-    if(x<0)
-    {   
-    reverse= reverse * -1;
-    }
-    if(reverse == x)
-    {
-   return 1;
-    }
-    else
-    {
-   return 0;
-    }
+    //printf("Reverse is %d and copy is %d\n", reverse, copy);
+    return reverse == copy;
 
 }  
 int main()
@@ -37,13 +29,14 @@ int main()
     printf("Enter the number\n");
     scanf("%d", &x);
     y = is_palindrome(x);
-    if (y == 1)
+    //printf("Y is %d\n", y);
+    if (y)
     {
-        printf("It is a palindrome");
+        printf("It is a palindrome\n");
     }
     else
     {
-        printf("No it is not a palindrome");
+        printf("No it is not a palindrome\n");
     }
 }
 
