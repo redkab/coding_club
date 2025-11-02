2fe0265d67de0fc03c9415ad178b76c39163dc0d
Saketh Indurthi <saketh.indurthi@gmail.com>
2025-11-02 01:39:37 -0500

    update string_to_integer.c

--- string_to_integer.c
+++ string_to_integer.c
@@ -2,7 +2,7 @@
 
 int myAtoi(char s[])
 {
-    int i,num=0, sign=1, digit, start, end;
+    int i,num=0, sign=1, digit, count=0, end;
     printf("Entered myAtoi with %s\n", s);
     for(i=0; s[i] != '\0'; i++)
     {
@@ -15,6 +15,15 @@ int myAtoi(char s[])
         {
             digit = s[i] - '0';
             num = num*10 + digit;
+            count++;
+
+        }
+        if(s[i] > '9' || s[i] <'0')
+        {
+            if(count >0)
+            {
+                return sign*num;
+            }
         }
     }
     
@@ -26,14 +35,7 @@ int myAtoi(char s[])
 
 int main()
 {
-    char num[] = "-042";
-    char *nums[] = {"-042", "        55", "       -0056", "-0000566", "abc123", "-abc246", "234abc"};
-    int i;
+    char num[] = "-042ab3";
     printf("%d\n", myAtoi(num));
-    for(i=0; i<sizeof(nums)/sizeof(num[0]); i++)
-    {
-        printf("%d %d\n", i, myAtoi(nums[i]));
-    }
-     
 }
 
