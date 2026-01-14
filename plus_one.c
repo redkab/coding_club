2dd2150d9c3afa94be87bff594df4c5f0ba9d98d
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-01-14 08:14:12 -0500

    update plus_one.c

--- plus_one.c
+++ plus_one.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int *plus(int *digits, int size)
-{
+void print(int *arr, int size);
+int* plus(int* digits, int size, int* returnSize); 
+int* plus(int* digits, int size, int* returnSize) {
     int *ans = (int *)malloc((size+1)*sizeof(int));
     int dig, carry=0, i;
     for(i=size-1; i>=0; i--)
@@ -18,9 +19,22 @@ int *plus(int *digits, int size)
             carry = (carry + digits[i])/10;
         }
         ans[i+1] = dig;
+        print(ans, size+1);
     }
-    ans[i] = carry;
+    ans[i+1] = carry;
+    *returnSize = size+1;
+
+    if(ans[0]==0)
+    {
+        printf("return size is %d\n", *returnSize);
+        *returnSize--;
+        printf("return size is %d\n", *returnSize);
+        //*returnSize=size;
+        return &(ans[1]);
+    }
     return ans;
+    
+    
 }
 void print(int *arr, int size)
 {
@@ -33,7 +47,10 @@ void print(int *arr, int size)
 
 int main()
 {
-    int arr[] = {1,2,3};
+    int arr[] = {9,9,9};
+    int x;
     int size = sizeof(arr)/sizeof(arr[0]);
-    print(plus(arr, size), size+1);
+    int *ret = plus(arr, size, &x);
+    printf("x is %d\n", x);
+    print(ret, x);
 }
