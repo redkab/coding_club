88db94603d4497f2ee8cbf66bcfd6283577ff23d
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-02-01 01:47:09 -0500

    add find_complement.c

---
+++ find_complement.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+void print(int *arr, int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+}
+int comp(int num)
+{
+    int arr[32] = {0};
+    int i;
+    for(i=0; num; i++)
+    {
+        arr[i] = num%2;
+        num /= 2;
+        //printf("i is %d\n", i);
+    }
+    print(arr, 32);
+    int rev = 0;
+    for(int j=0; j<i; j++)
+    {
+        rev = rev *2 +(arr[j] + 1)%2;
+    }
+    return rev;
+}
+
+int main()
+{
+    printf("%d\n", comp(1));
+}
+
