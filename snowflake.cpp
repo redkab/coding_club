9766b21b581613fed763d86dc5892df654c451bf
Saketh Indurthi <saketh.indurthi@gmail.com>
2026-06-24 00:09:03 +0530

    add snowflake.cpp

---
+++ snowflake.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void count(vector<vector<int>>&al)
+{
+    unordered_map<int, int>m;
+    for(int i=0; i<al.size(); i++)
+    {
+        if(!m.contains(al[i].size()))
+        {
+            m[al[i].size()] = 1;
+        }
+        else m[al[i].size()]++;
+    }
+    int x, leaf=0, y;
+    for(const auto& [a, b] : m)
+    {
+        if(b==1)x=a;
+        if(a==1)leaf=b;
+    }
+    cout<<x<<' '<<leaf/x<<'\n';
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m, u, v;
+        cin>>n>>m;
+        vector<vector<int>>al(n);
+        for(int i=0; i<m; i++)
+        {
+            cin>>u>>v;
+            al[u].push_back(v);
+            al[v].push_back(u);
+        }
+        count(al);
+    }
+}
